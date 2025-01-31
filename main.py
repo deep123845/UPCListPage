6cf4d8cab058af43b5c06b3faa82be21f06cad32
Deep123845 <deep123845@gmail.com>
2025-01-31 15:46:04 -0500

    added reading products csv

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+import csv
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
 
-def create_pdf():
+def read_csv():
+    products = []
+
+    with open("data.csv", "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            products.append([row[1], row[9]])
+
+    products.pop(0)
+
+    return products
+
+
+def create_pdf(products):
     c = canvas.Canvas("output.pdf", pagesize=letter)
     c.drawString(100, 750, "Test")
     c.save()
 
 
+products = read_csv()
+print(products)
 create_pdf()
